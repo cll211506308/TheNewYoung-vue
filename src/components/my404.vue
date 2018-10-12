1cf6e854b9a066f62f1cc5e3d8a18da4bd36ca72
asus <578690086@qq.com>
2018-10-12 15:22:06 +0800

    1.1.1

---
+++ src/components/my404.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <h1>
+        404
+      </h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "my404"
+    }
+</script>
+
+<style scoped>
+
+</style>
