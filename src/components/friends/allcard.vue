25cef2d9652ac7b48af4df23c36b6fe46559fff7
asus <578690086@qq.com>
2018-10-13 14:52:28 +0800

    1.1.1

--- src/components/friends/allcard.vue
+++ src/components/friends/allcard.vue
@@ -1,12 +1,29 @@
 <template>
     <div>
-      这是全部帖子
+      {{ data }}
+      <h1>分割</h1>
+      {{shuzi}}
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
     export default {
-        name: "allcard"
+      data () {
+        return {
+          data: null,
+          shuzi:null
+        }
+      },created(){
+        this.$axios.get('friends').then(
+          ((res)=>{this.data = res.data.data})
+        ).catch(err=>{console.log(err)})
+
+
+        this.$axios.get('friends/count').then(
+          ((res)=>{this.shuzi = res.data.data})
+        ).catch(err=>{console.log(err)})
+      }
     }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router/index'
+//引入
+import Axios from 'axios';
+Axios.defaults.baseURL = 'http://127.0.0.1:3000';
+//给Vue原型挂载一个属性
+Vue.prototype.$axios = Axios;
 
 Vue.config.productionTip = false
 
