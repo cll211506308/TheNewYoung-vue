aebbaf6d121b343da88d189ae45cc3967042799c
chenglili <2877453844@qq.com>
2018-10-16 16:02:45 +0800

    路由

--- src/components/users/alldatas.vue
+++ src/components/users/alldatas.vue
@@ -1,7 +1,16 @@
 <template>
-  <div id="userdata">
-    这里是用户信息
-  </div>
+
+  <!--<div>
+           <ul class="nav nav-pills">
+             <router-link tag="li" active-class="active" role="presentation" to="/users/alldatas/2" exact><a>我的体质</a></router-link>
+             <router-link tag="li" active-class="active" role="presentation" to="/users/usersarticals/2" ><a>我的文章</a></router-link>
+             <router-link tag="li" active-class="active" role="presentation" to="/users/userscards/2" ><a>我的贴子</a></router-link>
+             <router-link tag="li" active-class="active" role="presentation" to="/users/usercollection/2" ><a>我的收藏</a></router-link>
+             <router-link tag="li" active-class="active" role="presentation" to="/userbodydata" ><a>体质数据填写</a></router-link>
+             <router-link tag="li" active-class="active" role="presentation" to="/useridentity" ><a>身份认证</a></router-link>
+           </ul>
+           <router-view class="myself"></router-view>
+         </div>-->
 
 </template>
 
@@ -12,7 +21,5 @@
 </script>
 
 <style scoped>
-  #userdata{
-    border: 1px solid red;
-  }
+
 </style>
