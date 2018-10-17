1bebbc8d3d12f84e3d45b1f193497a87fa323e01
chenglili <2877453844@qq.com>
2018-10-17 16:04:29 +0800

    修改了个人中心的路由以及个人中心的组件

---
+++ src/components/users/basicdata.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <el-container>
+      <el-aside width="20%" height="50%"><div class="userpic">头像</div></el-aside>
+      <el-main>
+        <h3 class="username">{{username}}</h3>
+
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "basicdata",
+      data(){
+          return {
+            username:null
+          }
+      },
+      created(){
+        this.$axios.get('users/username/1').then(
+          ((res)=>{this.username = res.data.data[0].username})
+        ).catch(err=>{console.log(err)})
+      }
+    }
+</script>
+
+<style scoped>
+  .userpic{
+    background: aqua;
+    position:relative;
+    width: 80%;
+    height: 80%;
+    left:10%;
+    top: -40%;
+  }
+
+</style>
---
+++ src/components/users/userhome.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>个人中心主页</div>
+
+</template>
+
+<script>
+    export default {
+        name: "userhome"
+    }
+</script>
+
+<style scoped>
+
+</style>
