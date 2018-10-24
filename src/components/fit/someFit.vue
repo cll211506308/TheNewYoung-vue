777576bdc4bbc156202d3cc493b9dab42d62d729
yyq <1114033973@qq.com>
2018-10-24 23:05:45 +0800

    三个界面布局vue

--- src/components/fit/someFit.vue
+++ src/components/fit/someFit.vue
@@ -1,36 +1,6 @@
 <template>
   <div>
     <!--<h2>这是关于{{sort}}的健康饮食的文章</h2>-->
-    <router-link to="/artical/4">
-      <div class="content">
-        <img src="http://img3.imgtn.bdimg.com/it/u=3971964603,2168204668&fm=26&gp=0.jpg">
-        <p></p>
-      </div>
-    </router-link>
-    <router-link to="/artical/4">
-      <div class="content">
-        <img src="http://img3.imgtn.bdimg.com/it/u=3971964603,2168204668&fm=26&gp=0.jpg">
-        <p></p>
-      </div>
-    </router-link>
-    <router-link to="/artical/4">
-      <div class="content">
-        <img src="http://img3.imgtn.bdimg.com/it/u=3971964603,2168204668&fm=26&gp=0.jpg">
-        <p></p>
-      </div>
-    </router-link>
-    <router-link to="/artical/4">
-      <div class="content">
-        <img src="http://img3.imgtn.bdimg.com/it/u=3971964603,2168204668&fm=26&gp=0.jpg">
-        <p></p>
-      </div>
-    </router-link>
-    <router-link to="/artical/4">
-      <div class="content">
-        <img src="http://img3.imgtn.bdimg.com/it/u=3971964603,2168204668&fm=26&gp=0.jpg">
-        <p></p>
-      </div>
-    </router-link>
     <el-pagination class="page" background layout="prev, pager, next" :total="1000"></el-pagination>
   </div>
 </template>
@@ -47,18 +17,5 @@
 </script>
 
 <style scoped>
-  .content{
-    width: 90%;
-    height: 180px;
-    border: 2px solid red;
-    float: left;
-    margin-top: 10px;
-  }
-  img{width: 50%;height: 100%;}
-  .page{
-    margin-left:auto;
-    width: 82%;
-    margin-top: 30px;
-    float: left;
-  }
+
 </style>
--- src/components/youngLife/someYoungLife.vue
+++ src/components/youngLife/someYoungLife.vue
@@ -1,36 +1,6 @@
 <template>
   <div>
     <!--<h2>这是关于{{sort}}的养生堂的文章</h2>-->
-    <router-link to="/artical/6">
-      <div class="content">
-        <img src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=423278704,2694633106&fm=26&gp=0.jpg">
-        <p></p>
-      </div>
-    </router-link>
-    <router-link to="/artical/6">
-      <div class="content">
-        <img src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=423278704,2694633106&fm=26&gp=0.jpg">
-        <p></p>
-      </div>
-    </router-link>
-    <router-link to="/artical/6">
-      <div class="content">
-        <img src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=423278704,2694633106&fm=26&gp=0.jpg">
-        <p></p>
-      </div>
-    </router-link>
-    <router-link to="/artical/6">
-      <div class="content">
-        <img src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=423278704,2694633106&fm=26&gp=0.jpg">
-        <p></p>
-      </div>
-    </router-link>
-    <router-link to="/artical/6">
-      <div class="content">
-        <img src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=423278704,2694633106&fm=26&gp=0.jpg">
-        <p></p>
-      </div>
-    </router-link>
     <el-pagination class="page" background layout="prev, pager, next" :total="1000"></el-pagination>
   </div>
 </template>
@@ -47,18 +17,5 @@ export default {
 </script>
 
 <style scoped>
-  .content{
-    width: 90%;
-    height: 180px;
-    border: 2px solid red;
-    float: left;
-    margin-top: 10px;
-  }
-  img{width: 50%;height: 100%;}
-  .page{
-    margin-left:auto;
-    width: 82%;
-    margin-top: 30px;
-    float: left;
-  }
+
 </style>
