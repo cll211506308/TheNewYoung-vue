3cd564cff162be1ceea0ac7c35b571915142f640
asus <578690086@qq.com>
2018-11-02 10:53:56 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -16,17 +16,17 @@ export default {
     'bottom-con':bottomcon
   }
 }
-console.log('%c 新Young生',' text-shadow:#FF0000 0 0 10px;\n' +
-  '    color:white;\n' +
-  '    font-size:60px')
-// console.log('%c 新Young生 ','text-shadow: 0 0 1px #999,    \n' +
-//   '             1px 1px 1px #888,    \n' +
-//   '             2px 2px 1px #777,    \n' +
-//   '             3px 3px 1px #666,    \n' +
-//   '             4px 4px 1px #555,    \n' +
-//   '             5px 5px 1px #444;   \n' +
-//   'background-color:red;   \n' +
-//   'color:white;  font-size:50px;border-radius: 10px;')
+// console.log('%c 新Young生',' text-shadow:#FF0000 0 0 10px;\n' +
+//   '    color:white;\n' +
+//   '    font-size:60px')
+console.log('%c 新Young生 ','text-shadow: 0 0 1px #999,    \n' +
+  '             1px 1px 1px #888,    \n' +
+  '             2px 2px 1px #777,    \n' +
+  '             3px 3px 1px #666,    \n' +
+  '             4px 4px 1px #555,    \n' +
+  '             5px 5px 1px #444;   \n' +
+  'background-color:red;   \n' +
+  'color:white;  font-size:50px;border-radius: 10px;')
 </script>
 
 <style>
